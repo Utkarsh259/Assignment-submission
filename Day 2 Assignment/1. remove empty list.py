70a388f0d08f9bcbfb0e4b9380167546a25388ad
Utkarsh259 <77830244+Utkarsh259@users.noreply.github.com>
2021-01-22 15:00:15 +0530

    Day 2 assignment submitted

---
+++ Day 2 Assignment/1. remove empty list.py
@@ -0,0 +1,3 @@
+list1 = [[], "hii", [], [], [], 'hello', 'text', []]
+list2 = [e for e in list1 if e]
+print(list2)
---
+++ Day 2 Assignment/2. character position finder.py
@@ -0,0 +1,5 @@
+text = "hello, how are you"
+char = "h"
+for i in range(len(text)):
+    if (text[i] == char ):
+        print(char, "is in position ",i +1)
